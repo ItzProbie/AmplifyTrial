e425030f84bf3cd749f51a3b85c3b24925c9790f
Piyush Kulkarni <142049685+ItzProbie@users.noreply.github.com>
2024-10-11 09:17:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      // <header className="App-header">
+      //   <h1 className="name-title">Piyush Kulkarni</h1>
+      //   <h2 className="batch-info">Batch Q8</h2>
+      //   <p className="purpose-text">
+      //     This application was created for the sole purpose of 
+      //     Cloud Computing practical of deploying an app on AWS Amplify
+      //   </p>
+      //   <p className="intro-text">
+      //     Welcome to my React application!
+      //   </p>
+      //   <a
+      //     className="App-link"
+      //     href="https://reactjs.org"
+      //     target="_blank"
+      //     rel="noopener noreferrer"
+      //   >
+      //     Learn React
+      //   </a>
+      // </header>
       <header className="App-header">
-        <h1 className="name-title">Piyush Kulkarni</h1>
+        <h1 className="name-title">Shrikant Kshirsagar</h1>
         <h2 className="batch-info">Batch Q8</h2>
-        <p className="purpose-text">
-          This application was created for the sole purpose of 
-          Cloud Computing practical of deploying an app on AWS Amplify
+        <p className="">
+          Roll No : 42432
         </p>
         <p className="intro-text">
-          Welcome to my React application!
+          Welcome
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Learn React
         </a>
       </header>
